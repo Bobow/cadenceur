9c451cd52d5571dc27bb4ea5c3ee53949d24db48
Bobow <bjaurena@octo.com>
2010-12-20 16:58:38 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ITimeGuard::Application.config.secret_token = 'ee72896554b7bf89144ff514b690de37ec1868b51af8865def93519910cfeede848094353ef83d43dc1eb86953c64f18a8253b4f529eb311793a9a4dee5ef401'
